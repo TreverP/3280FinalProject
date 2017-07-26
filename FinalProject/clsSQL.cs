f93706f5cb05dec1ced2eba0cf47b55126cde3b6
Ravil Mukhamedov <chirachichira@gmail.com>
2017-07-26 10:19:38 -0600

    Added SearchWindow, SQL class, and DataAccess class

---
+++ FinalProject/clsSQL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280GP
+{
+    /// <summary>
+    /// Class to hold all the SQL statements for accessing the database
+    /// </summary>
+    class clsSQL
+    {
+        public clsSQL() {
+
+        }
+    }
+}
